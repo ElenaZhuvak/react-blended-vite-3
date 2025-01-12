0808dcc5f0288c74fc783091295015b71c9bee28
Elena Zhuvak <elenkazh@gmail.com>
2025-01-12 10:21:26 +0000

    adds Search page

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,15 +1,45 @@
-// import { FiSearch } from 'react-icons/fi';
+import styles from './SearchForm.module.css'
+import { FiSearch } from 'react-icons/fi';
+import {useSearchParams} from 'react-router-dom';
 
-// const regions = [
-//   { id: 'africa', value: 'africa', name: 'Africa' },
-//   { id: 'america', value: 'america', name: 'America' },
-//   { id: 'asia', value: 'asia', name: 'Asia' },
-//   { id: 'europe', value: 'europe', name: 'Europe' },
-//   { id: 'oceania', value: 'oceania', name: 'Oceania' },
-// ];
+const regions = [
+  { id: 'africa', value: 'africa', name: 'Africa' },
+  { id: 'america', value: 'america', name: 'America' },
+  { id: 'asia', value: 'asia', name: 'Asia' },
+  { id: 'europe', value: 'europe', name: 'Europe' },
+  { id: 'oceania', value: 'oceania', name: 'Oceania' },
+];
 
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const [, setSearchParams] = useSearchParams()
+
+ const handleSubmit = (e) => {
+  e.preventDefault()
+  const region = e.target.elements.region.value;
+  setSearchParams({reg: region})
+ }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <button className={styles.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <select
+        aria-label="select"
+        className={styles.select}
+        name="region"
+        required
+        defaultValue="default"
+      >
+        <option disabled value="default">
+          Select a region
+        </option>
+        {regions.map(region =><option key={region.id} value={region.value}>{region.value}</option> )}
+        
+      </select>
+    </form>
+  );
 };
 
 export default SearchForm;
--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,12 +1,43 @@
+import { useSearchParams } from 'react-router-dom';
 import Container from '../components/Container/Container';
-import Heading from '../components/Heading/Heading';
+import SearchForm from '../components/SearchForm/SearchForm';
 import Section from '../components/Section/Section';
+import { useEffect, useState } from 'react';
+import { fetchByRegion } from '../service/countryApi';
+import CountryList from '../components/CountryList/CountryList';
+import Loader from '../components/Loader/Loader';
 
 const SearchCountry = () => {
+  const [searchParams] = useSearchParams()
+  const region = searchParams.get('reg')
+  console.log(region)
+  const [countries, setCountries] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!region) {
+      return
+    }
+    const asyncWrapper = async () => {
+      setLoading(true);
+      try {
+        const response = await fetchByRegion(region);
+        setCountries(response);
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    asyncWrapper();
+  }, [region]);
+
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <SearchForm />
+        {countries && <CountryList countries={countries} />}
+        {loading && <Loader />}
       </Container>
     </Section>
   );
