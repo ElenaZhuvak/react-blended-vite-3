61157f0851533a66b78f6c0db3d817c7ccaf427e
olexiy-kireyko <a.kireyko@gmail.com>
2025-01-12 11:29:02 +0200

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
-import Heading from './components/Heading/Heading';
+// import Heading from './components/Heading/Heading';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import Loader from './components/Loader/Loader';
+const Home = lazy(() => import('./pages/Home'));
+const SearchCountry = lazy(() => import('./pages/SearchCountry'));
+const Country = lazy(() => import('./pages/Country'));
+import Header from './components/Header/Header';
 
 export const App = () => {
-  return <Heading title="App" bottom />;
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country" element={<SearchCountry />} />
+          <Route path="/country/:countryId" element={<Country />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
 };
